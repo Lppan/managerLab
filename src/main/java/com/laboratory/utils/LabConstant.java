a7ba3331c24c493075ed3e9bbb9bff883d499cc4
Lpan <span4718@163.com>
2017-12-25 16:15:40 +0800

    login message constant

---
+++ src/main/java/com/laboratory/utils/LabConstant.java
@@ -0,0 +1,25 @@
+package com.laboratory.utils;/**
+ * Created by Administrator on 2017/12/25.
+ */
+
+/**
+ * lab constant
+ * Created by Lpan on 2017/12/25.
+ */
+public class LabConstant {
+    public static class LabLogin{
+        public static final int LOGIN_SUCCESS_CODE = 0000;
+        public static final int LOGIN_FAILED_CODE = 0001;
+        public static final int LOGIN_ERROR_CODE = 0002;
+        public static final int LOGIN_EMPTY_CODE = 0003;
+        public static final String LOGIN_SUCCESS = "SUCCESS";
+        public static final String LOGIN_FAILED= "FAILD";
+        public static final String LOGIN_ERROR= "ERROR";
+        public static final String LOGIN_EMPTY= "EMPTY";
+        public static final String LOGIN_SUCCESS_MESSAGE = "登录成功";
+        public static final String LOGIN_FAILED_MESSAGE = "用户名或密码不正确";
+        public static final String LOGIN_ERROR_MESSAGE = "用户不存在";
+        public static final String LOGIN_EMPTY_MESSAGE = "用户不或密码不能为空";
+
+    }
+}
