c287aae495c755bbfae07778670dcf70c968c2cb
Lpan <span4718@163.com>
2017-12-25 16:16:21 +0800

    login

--- src/main/java/com/laboratory/system/user/controller/UserIndexController.java
+++ src/main/java/com/laboratory/system/user/controller/UserIndexController.java
@@ -1,9 +1,12 @@
 package com.laboratory.system.user.controller;
-
+import com.alibaba.fastjson.JSONObject;
+import com.laboratory.labport.model.ResponseModel;
 import com.laboratory.system.user.model.User;
 import com.laboratory.system.user.service.UserService;
+import com.laboratory.utils.LabConstant;
 import com.laboratory.utils.MD5Utils;
 import com.laboratory.utils.MapUtils;
+import org.apache.log4j.Logger;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
@@ -11,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -23,6 +27,9 @@ import java.util.Map;
 @Controller
 @RequestMapping("/system")
 public class UserIndexController {
+
+    private Logger logger = Logger.getLogger(UserIndexController.class);
+
     @Autowired
     private UserService userService;
 
@@ -31,8 +38,10 @@ public class UserIndexController {
      * @param request
      * @return
      */
-    @RequestMapping("/login")
+    @RequestMapping(value= "/login" , produces = "text/html;charset=UTF-8")
+    @ResponseBody
     public String login(HttpServletRequest request){
+        ResponseModel responseModel = new ResponseModel();
         Map<String, String> mapParams = MapUtils.getMapParams(request);
         boolean flag = MapUtils.isEmptyS(mapParams);
         //map 不为空
@@ -40,24 +49,50 @@ public class UserIndexController {
             //根据用户名查询是否有该用户
             String username = mapParams.get("userName");
             String password = mapParams.get("password");
-            List<User> users = userService.selectUserByUserName(mapParams);
-            if(null != users && users.size() > 0){
-                User user = users.get(0);
-                if(user.getPassword().equals(MD5Utils.encode(password)) && user.getUserName().equals(username)){
-                    //用户名密码正确
-                    UsernamePasswordToken token = new UsernamePasswordToken(username, password);
-                    Subject currentUser = SecurityUtils.getSubject();
-                    currentUser.login(token);
+            logger.info("登录用户名："+username+"登录密码："+password);
+            if(null != username &&  !"".equals(username) && null != password && !"".equals(password)){
+                try{
+                    List<User> users = userService.selectUserByUserName(mapParams);
+                if(null != users && users.size() > 0){
+                    User user = users.get(0);
+                    if(user.getPassword().equals(MD5Utils.encode(password)) && user.getUserName().equals(username)){
+                        //用户名密码正确
+    //                    UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+    //                    Subject currentUser = SecurityUtils.getSubject();
+    //                    currentUser.login(token);
+                        //获取权限信息
 
+                        responseModel.setStatus(LabConstant.LabLogin.LOGIN_SUCCESS_CODE);
+                        responseModel.setCode(LabConstant.LabLogin.LOGIN_SUCCESS);
+                        responseModel.setMessage(LabConstant.LabLogin.LOGIN_SUCCESS_MESSAGE);
+
+                    }else{
+                        //用户名密码不正确
+                        responseModel.setStatus(LabConstant.LabLogin.LOGIN_FAILED_CODE);
+                        responseModel.setCode(LabConstant.LabLogin.LOGIN_FAILED);
+                        responseModel.setMessage(LabConstant.LabLogin.LOGIN_FAILED_MESSAGE);
+                    }
                 }else{
-                    //用户名密码不正确
-
-
-
+                    //用户不存在
+                    responseModel.setStatus(LabConstant.LabLogin.LOGIN_ERROR_CODE);
+                    responseModel.setCode(LabConstant.LabLogin.LOGIN_ERROR);
+                    responseModel.setMessage(LabConstant.LabLogin.LOGIN_ERROR_MESSAGE);
                 }
+                }catch (Exception e){
+                    responseModel.setStatus(LabConstant.LabLogin.LOGIN_FAILED_CODE);
+                    responseModel.setCode(LabConstant.LabLogin.LOGIN_FAILED);
+                    responseModel.setMessage(LabConstant.LabLogin.LOGIN_FAILED_MESSAGE);
+                }
+            }else{
+                    //用户名或密码不能为空
+                responseModel.setStatus(LabConstant.LabLogin.LOGIN_EMPTY_CODE);
+                responseModel.setCode(LabConstant.LabLogin.LOGIN_EMPTY);
+                responseModel.setMessage(LabConstant.LabLogin.LOGIN_EMPTY_MESSAGE);
             }
         }
-        return null;
+        String jsonString = JSONObject.toJSONString(responseModel);
+        logger.info(jsonString);
+        return jsonString;
     }
 
 
