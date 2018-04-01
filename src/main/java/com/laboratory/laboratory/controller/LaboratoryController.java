cf8e74670e7fa116684bf8aa74d18d820c68840d
span <span4718@163.com>
2018-04-01 17:23:30 +0800

    update

--- src/main/java/com/laboratory/laboratory/controller/LaboratoryController.java
+++ src/main/java/com/laboratory/laboratory/controller/LaboratoryController.java
@@ -3,6 +3,7 @@ package com.laboratory.laboratory.controller;
 import com.laboratory.laboratory.model.Laboratory;
 import com.laboratory.laboratory.service.LaboratoryService;
 import com.laboratory.labport.model.ResponseModel;
+import com.laboratory.utils.LabConstant;
 import net.sf.json.JSONObject;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,35 +26,50 @@ public class LaboratoryController {
     @Autowired
     private LaboratoryService laboratoryService;
 
-    @RequestMapping("/showLabList")
-    public String ShowLabList(HttpServletRequest request,Map<String,Object> paramMap){
+    @RequestMapping(value = "/list")
+    public String ShowLabList(HttpServletRequest request, @RequestBody  Map<String,Object> paramMap){
         ResponseModel responseModel = laboratoryService.selectLaboratoryAllByPage(paramMap, request);
         JSONObject jsonObject = JSONObject.fromObject(responseModel);
         return jsonObject.toString();
     }
 
-    @RequestMapping("addLaboratory")
+    @RequestMapping(value = "add")
     public String addLaboratory(HttpServletRequest request, @RequestBody Laboratory laboratory){
         ResponseModel responseModel = laboratoryService.insertSelective(laboratory);
         JSONObject jsonObject = JSONObject.fromObject(responseModel);
         return jsonObject.toString();
     }
 
-    @RequestMapping("/updateLaboratory")
+    @RequestMapping(value = "detail")
+    public String selectById(HttpServletRequest request,@RequestBody Map<String,Object> paramMap){
+        ResponseModel responseModel = new ResponseModel();
+        if (null != paramMap && paramMap.containsKey("id")){
+            Integer id = Integer.valueOf(paramMap.get("id").toString());
+            logger.info("根据id查询实验室，id:" + id);
+            responseModel = laboratoryService.selectByPrimaryKey(id);
+        }else{
+            responseModel.setStatus(LabConstant.operateModel.OPERATE_EMPTY_STATUS);
+            responseModel.setMessage(LabConstant.operateModel.OPERATE_EMPTY_MUST_MESSAGE + ":id不能为空");
+        }
+        JSONObject jsonObject = JSONObject.fromObject(responseModel);
+        return jsonObject.toString();
+    }
+
+    @RequestMapping(value = "/update")
     public String updateLaboratory(HttpServletRequest request,@RequestBody Laboratory laboratory){
         ResponseModel responseModel = laboratoryService.updateByPrimaryKeySelective(laboratory);
         JSONObject jsonObject = JSONObject.fromObject(responseModel);
         return jsonObject.toString();
     }
 
-    @RequestMapping("/updateStatusUp")
+    @RequestMapping(value = "/updateStatusUp")
     public String updateStatusUp(HttpServletRequest request,@RequestBody Integer id){
         ResponseModel responseModel = laboratoryService.updateStatusUp(id);
         JSONObject jsonObject = JSONObject.fromObject(responseModel);
         return jsonObject.toString();
     }
 
-    @RequestMapping("/updateStatusDown")
+    @RequestMapping(value = "/updateStatusDown")
     public String updateStatusDown(HttpServletRequest request,@RequestBody Integer id){
         ResponseModel responseModel = laboratoryService.updateStatusDown(id);
         JSONObject jsonObject = JSONObject.fromObject(responseModel);
